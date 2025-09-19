679e1973da10d45d4ec1896779f459e10c999e10
NoeRebourg <noe.rebourg@iut-rodez.fr>
2025-09-19 08:13:15 +0000

    suite du tp 1 avec le read_txt dans la classe Image

--- lectureEcritureJava/Main.java
+++ lectureEcritureJava/Main.java
@@ -1,15 +1,17 @@
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 
 public class Main {
     public static void main(String[] args) {
-        String enTete = "P3\n100 100\n255\n";
-
         try {
-            RandomAccessFile raf = new RandomAccessFile("firstPPM_withRAF.ppm", "rw");
-            raf.writeBytes(enTete.getBytes()); // écrit le texte en octets
+            FileInputStream fis = new FileInputStream("firstPPM.ppm")
+            byte[] buffer = new byte[128];
+            int bytesRead = fis.read(buffer);
+            System.out.println(new String(buffer, 0, bytesRead));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+
 }
